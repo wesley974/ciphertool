4b117dd921b505659dd537e9f09fce146e69ee81
Wesley <milo974@gmail.com>
2016-02-16 10:36:55 +0400

    Fix variable ef to e, new function run()

--- ciphertool.c
+++ ciphertool.c
@@ -30,15 +30,16 @@
 #define OPT1 "enc -aes-256-cbc -salt -in" // Option 1 : Encrypt
 #define OPT2 "enc -aes-256-cbc -d -in" // Option 2 : Decrypt
 
+char openssl_cmd[4096];
+
 int
 main(int argc, char *argv[])
 {
     char *nfile;
-    char openssl_cmd[4096];
     char rm_cmd [4096];
     char *p;
     FILE *ifile;
-    int ef;
+    int e;
 
     if (argc == 1) {
         usage();
@@ -49,16 +50,16 @@ main(int argc, char *argv[])
         errx(1,"Bad password");
     }
 
-    for(ef=1; ef<argc; ef++){
+    for(e=1; e<argc; e++){
 
-        printf("-- File to process : %s\n",argv[ef]);
+        printf("-- File to process : %s\n",argv[e]);
 
-        ifile=fopen(argv[ef],"r");
+        ifile=fopen(argv[e],"r");
         if (ifile==NULL){
                 errx(1,"File error");
         }
 
-        char *ext = strrchr(argv[ef],'.');
+        char *ext = strrchr(argv[e],'.');
 
         if (ext==NULL){
                 ext="null";
@@ -66,19 +67,17 @@ main(int argc, char *argv[])
 
         if (strcmp(ext,EXT)==0){
             printf("Decrypting...");
-            nfile=argv[ef];
+            nfile=argv[e];
             nfile[strlen(nfile)-4]='\0';
             snprintf(openssl_cmd,4096,"%s %s %s%s -out %s -pass pass:%s > /dev/null 1>&1",OPENSSL,OPT2,nfile,EXT,nfile,HASH);
-            system(openssl_cmd);
-	    printf("OK\n");
+	    run();
 
         } else {
             printf("Encrypting...");
-            snprintf(openssl_cmd,4096,"%s %s %s -out %s%s -pass pass:%s > /dev/null 1>&1",OPENSSL,OPT1,argv[ef],argv[ef],EXT,HASH);
-            system(openssl_cmd);
-            snprintf(rm_cmd,4096,"rm -f %s",argv[ef]);
+            snprintf(openssl_cmd,4096,"%s %s %s -out %s%s -pass pass:%s > /dev/null 1>&1",OPENSSL,OPT1,argv[e],argv[e],EXT,HASH);
+	    run();
+            snprintf(rm_cmd,4096,"rm -f %s",argv[e]);
             system(rm_cmd);
-	    printf("OK\n");
         }
 
 
@@ -93,4 +92,10 @@ usage(void)
     (void)fprintf(stderr,
     "usage: ciphertool file ...\n");
     exit(1);
+}
+
+run(void)
+{
+    system(openssl_cmd);
+    printf("OK\n");
 }
